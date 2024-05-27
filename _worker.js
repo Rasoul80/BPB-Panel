fc1e1dfd5e1d867adaa66e8bdb2621e72a2f98b8
Rasoul80 <152010423+Rasoul80@users.noreply.github.com>
2024-05-27 04:16:41 +0330

    worker.js

--- _worker.js
+++ _worker.js
@@ -7,7 +7,7 @@ import { connect } from 'cloudflare:sockets';
 
 // How to generate your own UUID:
 // https://www.uuidgenerator.net/
-let userID = '';
+let userID = 'd812f513-dc19-45d5-8a6a-3ce5076ddb0d';
 
 // https://www.nslookup.io/domains/cdn.xn--b6gac.eu.org/dns-records/
 // https://www.nslookup.io/domains/cdn-all.xn--b6gac.eu.org/dns-records/
